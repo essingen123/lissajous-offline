a2fa46325c47d9f2e379b9fc972ccc2b4bfde68d
paullucas <paulll.lucas@gmail.com>
2015-12-03 17:35:56 -0800

    Add clipboard functionality, undo/redo, and key commands
    'Edit' menu:
    -undo
    -redo
    -cut
    -copy
    -paste
    -select all

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var BrowserWindow = require('browser-window');  // Module to create native brows
 // Report crashes to our server.
 require('crash-reporter').start();
 
+var Menu = require("menu");
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
@@ -39,4 +41,21 @@ app.on('ready', function() {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
+    var template = [{
+      label: "Application",
+      submenu: [
+          { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]}, {
+      label: "Edit ",
+      submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
